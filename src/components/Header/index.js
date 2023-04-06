736a1576ae3eb455f933ad27d13d63826e7beb36
Leo Nogueira <leo.nogueira@webmix.dev>
2023-04-06 20:58:02 -0300

    New photo and particles scheme

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import './Header.css';
 import { 
     FaGithub, FaLinkedinIn, FaWhatsapp 
 } from "react-icons/fa";
-import photo from './photo.jpeg';
+import photo from './5.jpg';
 import MySkills from '../MySkills';
 
 const Header = () => {
@@ -16,7 +16,7 @@ const Header = () => {
             <span>Fullstack Developer /&nbsp;</span><MySkills/><br/><hr/>
             </h5>
             <h5 className='main-links'>
-                <a href="https://www.linkedin.com/in/leo-maceroux-b61294187/" target="blank"><FaLinkedinIn/></a>
+                <a href="https://www.linkedin.com/in/leonardo-nogueira-b61294187/" target="blank"><FaLinkedinIn/></a>
                 <a href="https://github.com/leo-nog" target="blank"><FaGithub/></a>
                 <a href="https://wa.me/5511963060259" target="blank"><FaWhatsapp/></a>
             </h5>
